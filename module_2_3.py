9a6eb1b37c60ec5a3709bae438d492e0fa313c88
Mtsyr2 <mtsyr@yandex.ru>
2024-11-08 08:53:05 +0300

    2_0

---
+++ module_2_3.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+while True:
+    for i in my_list:
+        if i == 0:
+            continue
+        elif i > 0:
+            print(i)
+        else:
+            break
