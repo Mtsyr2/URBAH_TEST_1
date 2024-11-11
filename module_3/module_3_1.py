d696b7b1978a91f9c5c76b82038c947944e22a09
Macienko.K <mtsyr@yandex.ru>
2024-11-11 13:25:30 +0300

    3_1

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -1,1 +1,28 @@
 
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls+=1
+
+
+def string_info(string: str)->tuple:
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string: str, list_to_search: list)->bool:
+    count_calls()
+    if string.lower() in [item.lower() for item in list_to_search]:
+        return True
+    else:
+        return False
+
+
+print(string_info('Identification'))
+print(string_info('Percussion'))
+print(is_contains('МиФ', ['мирР', 'МаФин', 'миФический', 'миф']))
+print(is_contains('lab', ['AlaBama', 'Labor', 'GiBraltar']))
+print(calls)
+
