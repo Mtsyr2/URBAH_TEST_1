1b3cde40ebb42899f3e5ed61573683cf8969902d
Macienko.K <mtsyr@yandex.ru>
2024-11-15 16:22:29 +0300

    3_hard

---
+++ module_3/module_3_hard.py
@@ -0,0 +1,22 @@
+
+def calculate_structure_sum(list_: list):
+    pass
+
+
+
+
+
+# data_structure = [
+# [1, 2, 3],
+# {'a': 4, 'b': 5},
+# (6, {'cube': 7, 'drum': 8}),
+# "Hello",
+# ((), [{(2, 'Urban', ('Urban2', 35))}])
+# ]
+#
+# result = calculate_structure_sum(data_structure)
+# print(result)
+
+
+
+print(isinstance([1, 2, 3], tuple))
