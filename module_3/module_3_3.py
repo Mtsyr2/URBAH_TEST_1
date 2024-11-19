ab19adec84ff804a11e210dde15a4dc801dca70b
Mtsyr2 <mtsyr@yandex.ru>
2024-11-19 19:10:18 +0300

    3_hard_2

--- module_3/module_3_3.py
+++ module_3/module_3_3.py
@@ -8,14 +8,16 @@ values_dict = {'a': 34, 'b': 'Phillip', 'c': [1, 2, 5]}
 values_list2 = [56.17, 'John']
 
 
-print_params()
-print_params(a=12)
-print_params(a=True, b=17)
-print_params(a=45.34, b='cdr', c=False)
-print_params(b=25)
-print_params(c=[1, 2, 3])
+# print_params()
+# print_params(a=12)
+# print_params(a=True, b=17)
+# print_params(a=45.34, b='cdr', c=False)
+# print_params(b=25)
+# print_params(c=[1, 2, 3])
+#
+# print_params(*values_list)
+# print_params(**values_dict)
+# print_params(*values_list2, 42)
 
-print_params(*values_list)
-print_params(**values_dict)
-print_params(*values_list2, 42)
-
+e = values_dict.values()
+print(type(e))
--- module_3/module_3_hard.py
+++ module_3/module_3_hard.py
@@ -1,22 +1,39 @@
 
 def calculate_structure_sum(list_: list):
-    pass
+    sum_ = 0
+    for item in list_:
+        if isinstance(item, tuple):
+            for i in item:
+                calculate_structure_sum(list(item))
+        elif isinstance(item, dict):
+            calculate_structure_sum(list(item))
+            calculate_structure_sum(list(item.values()))
+        elif isinstance(item, list):
+            calculate_structure_sum(item)
+        elif isinstance(item, str):
+            sum_+=len(item)
+        elif isinstance(item, int):
+            sum_+=item
+        elif isinstance(item, float):
+            sum_ += item
+
+        else:
+            return sum_
+    return sum_
 
 
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+    ]
+
+result = calculate_structure_sum(data_structure)
+print(result)
 
 
-
-# data_structure = [
-# [1, 2, 3],
-# {'a': 4, 'b': 5},
-# (6, {'cube': 7, 'drum': 8}),
-# "Hello",
-# ((), [{(2, 'Urban', ('Urban2', 35))}])
-# ]
 #
-# result = calculate_structure_sum(data_structure)
-# print(result)
-
-
-
-print(isinstance([1, 2, 3], tuple))
+# # print(isinstance([1, 2, 3], tuple))
+# print(sum([1,2,3]))
