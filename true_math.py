9941861c361cd3081c27dabc2e1e99aeff79a71d
Mtsyr2 <mtsyr@yandex.ru>
2024-11-20 19:16:29 +0300

    4_1

---
+++ true_math.py
@@ -0,0 +1,16 @@
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first/second
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
