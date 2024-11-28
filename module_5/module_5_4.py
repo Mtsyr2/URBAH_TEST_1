bba199cf90cabb3a5093e564d9ff96cb8c51142d
Macienko.K <mtsyr@yandex.ru>
2024-11-28 11:49:38 +0300

    5_3_3

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -1,14 +1,20 @@
+from typing import List, Any
+
 
 class House:
-    houses_history = []
+    houses_history: list = []
 
-    def __new__(cls, *args, **kwargs):
-       # houses_history
-        return object.__new__()
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+
+        return object.__new__(cls)
 
     def __init__(self, name, number_of_floor):
         self.name = name
         self.number_of_floor = number_of_floor
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
 
     def go_to(self, new_floor):
         if 1 < new_floor<= self.number_of_floor:
@@ -131,26 +137,16 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2)  # __eq__
+print(House.houses_history)
 
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10  # __iadd__
-print(h1)
-
-h2 = 10 + h2  # __radd__
-print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
 
