a03e5ba77537e84caa182b746b3de1a85371814e
Macienko.K <mtsyr@yandex.ru>
2024-11-28 15:14:08 +0300

    5_hard_i

---
+++ module_5/module5hard.py
@@ -0,0 +1,28 @@
+
+class User:
+
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = password
+        self.age = age
+
+
+class Video:
+
+    def __init__(self, title: str, duration: int, time_now: int, adult_mode: bool):
+        self.title = title
+        self.duration = duration
+        self. time_now = time_now
+        self.adult_mode = adult_mode
+
+
+class UrTube:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
