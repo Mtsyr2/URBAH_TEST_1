a3a0bf4ad3381ff1a61336e11d9dd4fa1d88ad3f
Mtsyr2 <mtsyr@yandex.ru>
2024-11-29 16:06:42 +0300

    hard5

--- module_5/module5hard.py
+++ module_5/module5hard.py
@@ -82,6 +82,7 @@ class UrTube:
     def watch_video(self, title):
         if not self.current_user:
             print('Войдите в аккаунт, чтобы смотреть видео')
+            return
 
         video = None
         for v in self.videos:
@@ -108,29 +109,26 @@ def main():
     v1 = Video('Лучший язык программирования 2024 года', 200)
     v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-
-
     # Добавление видео
     ur.add(v1, v2)
 
-    #Проверка поиска
+    # Проверка поиска
     print(ur.get_videos('лучший'))
     print(ur.get_videos('ПРОГ'))
 
-    #Проверка на вход пользователя и возрастное ограничение
+    # Проверка на вход пользователя и возрастное ограничение
     ur.watch_video('Для чего девушкам парень программист?')
     ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-    print(ur.current_user)
     ur.watch_video('Для чего девушкам парень программист?')
     ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
     ur.watch_video('Для чего девушкам парень программист?')
 
-    # # Проверка входа в другой аккаунт
-    # ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-    # print(ur.current_user)
-    #
-    # # Попытка воспроизведения несуществующего видео
-    # ur.watch_video('Лучший язык программирования 2024 года!')
+    # Проверка входа в другой аккаунт
+    ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+    print(ur.current_user)
+
+    # Попытка воспроизведения несуществующего видео
+    ur.watch_video('Лучший язык программирования 2024 года!')
 
 
 if __name__ == '__main__':
