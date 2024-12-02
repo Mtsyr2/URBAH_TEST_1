c85d86dca0a1fbd3972491f469ef5ae13538f36c
Mtsyr2 <mtsyr@yandex.ru>
2024-12-02 18:50:22 +0300

    6.1

--- module_6/module_6_1.py
+++ module_6/module_6_1.py
@@ -2,19 +2,25 @@
 class Animal:
 
     def __init__(self):
+        self.food = None
         self.alive = True
         self.fed = False
+        self.name = None
 
     def eat(self, food):
         self.food = food
-
+        if self.food.edible:
+            print(f'{self.name} съел {self.food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {self.food.name}')
+            self.alive = False
 
 
 class Plant:
 
-    def __init__(self, name):
+    def __init__(self):
         self.edible = False
-        self.name = name
 
 
 class Mammal(Animal):
@@ -27,25 +33,42 @@ class Mammal(Animal):
 class Predator(Animal):
 
     def __init__(self, name):
-        self.alive = True
-        self.fed = False
+        super().__init__()
         self.name = name
 
 
 class Flower(Plant):
-    pass
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
 
 class Fruit(Plant):
-    pass
+
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.edible = True
 
 
 def main():
     a1 = Predator('Волк с Уолл-Стрит')
     a2 = Mammal('Хатико')
-    # p1 = Flower('Цветик семицветик')
-    #     # p2 = Fruit('Заводной апельсин')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
 
-    print(a1.name, a2.name)
+    print(a1.name)
+    print(p1.name)
+    print(a2.name)
+    print(p2.name)
+
+    print(a1.alive)
+    print(a2.fed)
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
 
 
 if __name__ == '__main__':
