a52b0451a9ef684bae7a90f39a2a91c071f865eb
Macienko.K <mtsyr@yandex.ru>
2024-12-03 11:50:10 +0300

    6_2_3

--- module_6/module_6_2.py
+++ module_6/module_6_2.py
@@ -8,27 +8,41 @@ class Vehicle:
         self.__color = __color
         self.__engine_power = __engine_power
 
-
     def get_model(self):
-        pass
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        pass
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        pass
+        return f'Цвет: {self.__color}'
 
+    def print_info(self):
+        print(f'Модель: {self.__model}\nМощность двигателя: {self.__engine_power}\nЦвет: {self.__color}\n'
+              f'Владелец: {self.owner}')
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
 
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
-    def __init__(self, *args):
-        super().__init__(self)
 
-
-
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
-print(vehicle1)
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
 
