35ab59826ba5fea8a06ac25b27602820a2677ca4
Macienko.K <mtsyr@yandex.ru>
2024-12-04 16:24:37 +0300

    6h

---
+++ module_6/module6hard.py
@@ -0,0 +1,22 @@
+class Figure:
+    pass
+
+
+class Circle:
+    pass
+
+
+class Triangle:
+    pass
+
+
+class Cube:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
