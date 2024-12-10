d367d5844ec45e783aed1864338d17f918ee7113
Macienko.K <mtsyr@yandex.ru>
2024-12-10 16:42:34 +0300

    7_4

--- module_7/module_7_5.py
+++ module_7/module_7_5.py
@@ -9,7 +9,7 @@ def main():
             filetime = os.path.getmtime(file)
             formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
             filesize = os.path.getsize(file)
-            parent_dir = os.chroot(file)
+            parent_dir = os.getcwd()
             print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
                   f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
