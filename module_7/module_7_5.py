c4eb01066d7052276250733fe433ccbad8f746cc
Macienko.K <mtsyr@yandex.ru>
2024-12-11 11:37:06 +0300

    7_5

--- module_7/module_7_5.py
+++ module_7/module_7_5.py
@@ -1,18 +1,18 @@
 import os
 import time
 
-def main():
-    directory = '.'
+
+def main(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            filepath = os.path.join(file)
+            filepath = os.path.join(root, file)
             filetime = os.path.getmtime(file)
             formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
             filesize = os.path.getsize(file)
-            parent_dir = os.getcwd()
+            parent_dir = os.path.dirname(os.path.abspath(file))
             print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
                   f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 
 
 if __name__ == '__main__':
-    main()
+    main('.')
