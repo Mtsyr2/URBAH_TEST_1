e9869a8b256a54ba1532e14dd36fa180b019a431
Mtsyr2 <mtsyr@yandex.ru>
2024-12-12 17:55:49 +0300

    9-3-

--- module_9/module_9_3.py
+++ module_9/module_9_3.py
@@ -3,7 +3,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (abs(len(x) - len(y)) for x, y in zip(first, second) if len(x) != len(y))
-second_result = (len(x[i]) == len(y[i]) for x in first for y in second for i in range(len(first)-1))
+second_result = (len(first[x]) == len(second[y]) for x in range(len(first)) for y in range(len(second)) if x==y)
 
 
 def main():
