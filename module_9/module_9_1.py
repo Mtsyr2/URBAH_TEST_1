82624b1c85a29f046048f7bc7d57a8e29038dc85
Macienko.K <mtsyr@yandex.ru>
2024-12-12 14:20:15 +0300

    9-1

---
+++ module_9/module_9_1.py
@@ -0,0 +1,15 @@
+
+def apply_all_func(int_list: (int, float), *functions):
+    results = {}
+    for func in functions:
+        results[func.__name__] = func(int_list)
+    return results
+
+
+def main():
+    print(apply_all_func([6, 20, 15, 9], max, min))
+    print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
+if __name__ == '__main__':
+    main()
