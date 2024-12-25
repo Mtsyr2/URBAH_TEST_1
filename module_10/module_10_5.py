31d6bc1cd82c56d13141fb194b4fc4e9889955a5
Macienko.K <mtsyr@yandex.ru>
2024-12-25 14:51:35 +0300

    10_5

--- module_10/module_10_5.py
+++ module_10/module_10_5.py
@@ -2,12 +2,6 @@
 from datetime import datetime
 from multiprocessing import Pool
 
-files_ = ['file 1.txt',
-          'file 2.txt',
-          'file 3.txt',
-          'file 4.txt'
-          ]
-
 
 def read_info(name):
     all_data = []
@@ -16,21 +10,18 @@ def read_info(name):
             all_data.append(ff.readline())
 
 
-def main():
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    # линейно
     t1 = datetime.now()
-    for file in files_:
+    for file in filenames:
         read_info(file)
     t2 = datetime.now()
     print(t2 - t1)
 
-
-if __name__ == '__main__':
-    # линейно
-    main()
-
     # многопроцессно
     t3 = datetime.now()
     with Pool(4) as p:
-        p.map(read_info, files_)
+        p.map(read_info, filenames)
     t4 = datetime.now()
     print(t4 - t3)
